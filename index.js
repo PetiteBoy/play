f63246587d9cc712c2b66c9e46528fb24d8be0f4
wangrunzhuang <wangrunzhuang@zuoyebang.com>
2020-08-14 17:08:29 +0800

    add file

--- index.js
+++ index.js
@@ -4,26 +4,32 @@ let http = require('http').createServer(app);
 
 let io = require('socket.io')(http);
 
+let userList = [];
+
+let onlineList = [];
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
 
 io.on('connect', (socket) => {
-  console.log('用户链接')
 
   // 新消息过来了
-  socket.on('chat message', (msg) => {
-    console.log('新消息过来了', msg)
+  socket.on('chat msg', (msg) => {
+    io.emit('chat msg', msg)
 
-    io.emit('chat message', msg)
-
-    socket.broadcast.emit('hi')
   })
+
+  socket.on('self online', (name) => {
+    userList.push(name);
+    console.log(name)
+    io.emit('user online', name)
+  })
+
   socket.on('disconnect', () => {
     console.log('用户断开链接')
   })
-
 
 })
 
